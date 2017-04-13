98d622068626ef88bb28509c793fd56ec05362aa
s.reddy.mettukuru <sivakmr469@gmail.com>
2017-04-13 16:48:33 +0530

    jenkins test

--- SpringSecHelloWorld/src/com/siva/spring/model/User.java
+++ SpringSecHelloWorld/src/com/siva/spring/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-/*this is user table in nfc*/
+/*this is user table in nfc1*/
 
 @Entity
 @Table(name="users")
