acb3ea4755547668a051c2228962342c381a480c
Reddy Mettukuru <sivakmr469@gmail.com>
2017-04-13 17:03:17 +0530

    sadfak

--- SpringSecHelloWorld/src/com/siva/spring/model/User.java
+++ SpringSecHelloWorld/src/com/siva/spring/model/User.java
@@ -10,7 +10,7 @@ import javax.persistence.FetchType;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-/*this is user table in nfc2*/
+/*this is user table in nfc3*/
 
 @Entity
 @Table(name="users")
